fa371089d96002f4fd551e015409fc3e3a5e6580
Javier Leguizamo <jlegui745@gmail.com>
2022-02-02 02:05:16 -0500

    Remove stuff

--- events.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-const client = require('./bot')
-const eventFiles = fs.readdirSync('./events')
-
-for (const file of eventFiles) {
-    const event = require(`./events/${file}`)
-    client.on(event.name, event.exec)
-}
