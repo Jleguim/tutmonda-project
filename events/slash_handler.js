efe60b38b435cd59ede4639895d74209f37e8c32
Javier Leguizamo <jlegui745@gmail.com>
2022-02-01 23:57:29 -0500

    Pass DB models to command

--- events/slash_handler.js
+++ events/slash_handler.js
@@ -1,4 +1,5 @@
 const client = require('../bot')
+const models = require('mongoose').models
 
 module.exports.name = 'interactionCreate'
 module.exports.exec = (interaction) => {
@@ -6,7 +7,7 @@ module.exports.exec = (interaction) => {
     if (!client._slash.has(interaction.commandName)) return
 
     try {
-        client._slash.get(interaction.commandName).exec(interaction, client)
+        client._slash.get(interaction.commandName).exec(interaction, client, models)
     } catch (error) {
         console.error(error)
         interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
