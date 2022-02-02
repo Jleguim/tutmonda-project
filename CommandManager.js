bc73a6985a11830ec38d75835676cb6db490f8fa
Javier Leguizamo <jlegui745@gmail.com>
2022-02-02 02:05:44 -0500

    New way of registering and handling commands

---
+++ CommandManager.js
@@ -0,0 +1,72 @@
+const { Routes } = require('discord-api-types/v9')
+const { REST } = require('@discordjs/rest')
+
+const Builders = require('@discordjs/builders')
+const models = require('mongoose').models
+const fs = require('fs')
+
+const rest = new REST({ version: '9' }).setToken(process.env.TOKEN)
+const route = (process.env.DEV == 'TRUE')
+    ? Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID)
+    : Routes.applicationCommands(process.env.CLIENT_ID)
+
+class CmdManager {
+    constructor(path = './commands/') {
+        this.path = path
+        this.commands = new Map()
+    }
+
+    loadAndRegister() {
+        this._loadCommands()
+        return this._registerCommands()
+    }
+
+    handleInteractions(inter) {
+        const { commandName } = inter
+
+        if (!inter.isCommand()) return
+        if (!this.commands.has(commandName)) return
+
+        var command = this.commands.get(commandName)
+        var params = {}
+
+        command.options.forEach(o => {
+            var n = o.name
+            params[n] = inter.options.get(n)
+        })
+
+        try {
+            command.exec(inter, models, params)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    _loadCommands() {
+        const files = fs.readdirSync(this.path)
+        files.map(f => {
+            const cmd = require(this.path + f)
+            cmd.info.aliases.forEach(alias => {
+                var slash = this._buildCommand(alias, cmd.info.description, cmd.options)
+                this.commands.set(slash.name, { ...cmd, slash })
+            })
+        })
+    }
+
+    _buildCommand(name, desc, options) {
+        var slashCommand = new Builders.SlashCommandBuilder()
+        slashCommand.name = name
+        slashCommand.description = desc
+        slashCommand.options = options
+        return slashCommand
+    }
+
+    _registerCommands() {
+        var commands = Array.from(this.commands.values())
+            .map(i => i = i.slash.toJSON())
+
+        return rest.put(route, { body: commands })
+    }
+}
+
+module.exports = CmdManager
--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 require('dotenv').config()
 
-const client = require('./bot')
-require('./events')
-require('./slash_commands')
+const Discord = require('discord.js')
+const Intents = Discord.Intents.FLAGS
 
-client.on('ready', () => {
+const client = new Discord.Client({ intents: [Intents.GUILDS, Intents.GUILD_MESSAGES] })
+
+const CmdManager = require('./CommandManager')
+const cmdManager = new CmdManager()
+
+client.on('interactionCreate', (i) => cmdManager.handleInteractions(i))
+
+client.once('ready', () => {
     console.log('Ready')
-
     require('./db')
 })
 
+cmdManager.loadAndRegister()
 client.login(process.env.TOKEN)
