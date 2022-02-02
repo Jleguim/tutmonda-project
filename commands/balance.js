29c5f886f4653acb9913f9684712d43e69b6efd7
Javier Leguizamo <jlegui745@gmail.com>
2022-02-02 02:05:54 -0500

    Balance command using rework

---
+++ commands/balance.js
@@ -0,0 +1,28 @@
+const { SlashCommandUserOption } = require('@discordjs/builders')
+
+module.exports.exec = async function (inter, models, params, client) {
+    const { Users, Transactions } = models
+    const { usuario } = params
+
+    var target = inter.user
+    if (usuario) target = usuario.user
+
+    var userDoc = await Users.getByUid(target.id)
+    if (!userDoc) userDoc = Users.create(target.id)
+
+    await userDoc.save()
+    inter.reply(`${userDoc.wallet.balance}`)
+}
+
+module.exports.info = {
+    name: 'Balance',
+    description: 'Muestra el balance de un usuario.',
+    aliases: ['bal', 'balance']
+}
+
+module.exports.options = [
+    new SlashCommandUserOption()
+        .setName('usuario')
+        .setDescription('Usuario a buscar.')
+        .setRequired(false)
+]
