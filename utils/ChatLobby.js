7cc9b2ab63c4f9edcb011b04c06b332786de246e
Javier Leguizamo <jlegui745@gmail.com>
2022-02-05 09:33:54 -0500

    QoL changes and addUser

--- utils/ChatLobby.js
+++ utils/ChatLobby.js
@@ -1,28 +1,37 @@
-// const { client } = require('../index')
-
 class ChatLobby {
     constructor(users) {
         this.users = users
+        this.collectors = new Map()
 
         this.users.forEach((data) => {
-            const { user, channel } = data
-
-            const filter = m => m.author.id == user.id
-            const collector = channel.createMessageCollector({ filter, time: 50000 })
-
-            data = { ...data, collector }
-
-            collector.on('collect', m => {
-                var others = this.filterOut(data)
-                this.sendMessages(others, m)
-            })
-
-            collector.on('end', () => {
-                channel.send('Interrupcion de conexión.')
-            })
+            this._setupCollector(data)
         })
     }
-    
+
+    _setupCollector(data) {
+        const { user, channel } = data
+
+        const filter = m => m.author.id == user.id
+        const collector = channel.createMessageCollector({ filter, time: 50000 })
+
+        this.collectors.set(user.id, collector)
+
+        collector.on('collect', m => {
+            var others = this.filterOut(data)
+            this.sendMessages(others, m)
+        })
+
+        collector.on('end', () => {
+            channel.send('Interrupcion de conexión.')
+        })
+    }
+
+    addUser(data) {
+        this.users.push(data)
+        this._setupCollector(data)
+        this.announce('New user joined.')
+    }
+
     announce(message) {
         this.users.forEach(data => {
             const { user, channel } = data
