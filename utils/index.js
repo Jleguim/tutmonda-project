915a81307e1f12ff724d8805482f365937c25f81
Javier Leguizamo <jlegui745@gmail.com>
2022-02-06 21:30:25 -0500

    Merge pull request #2 from Jleguim/Comunicador
    TextCalls

--- utils/index.js
+++ utils/index.js
@@ -4,11 +4,15 @@ const Functions = require('./Functions')
 const ChatLobby = require('./ChatLobby')
 const Queue = require('./Queue')
 
+function getRandomId() {
+    return ('000000000' + Math.random().toString(36).substr(2, 9)).slice(-9)
+}
+
 module.exports = {
     Timeout,
     XpManager,
-    
-    ...Functions,
     Queue,
-    ChatLobby
+    ChatLobby,
+
+    ...Functions
 }
